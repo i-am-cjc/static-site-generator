e9c0dbf4b7a8ee35fac6760f3c7c245cc13ebd71
Carl Clegg <carlclegg@gmail.com>
2016-07-06 23:42:22 +0100

    Added shoddy advisories section, TODO, improve shoddy advisories section

--- ssg.sh
+++ ssg.sh
@@ -3,23 +3,28 @@ FOOTER=_templates/footer.html
 PAGEHEADER=_templates/pageheader.html
 PAGEFOOTER=_templates/pagefooter.html
 INDEX=_output/index.html
+ADV=_output/advisories/index.html
 ARCHIVE=_output/archive/index.html
 PREPROCESSOR=Markdown.pl
-POSTCOUNT=10
+POSTCOUNT=6
 
 echo ">> Wiping _output"
 mkdir -p _output
 rm -rf _output/*
 mkdir -p _output/archive
+mkdir -p _output/advisories
 
 echo ">> Copying assets"
 cp -r _assets/* _output
 
 cat $HEADER > $INDEX
 cat $HEADER > $ARCHIVE
+cat $HEADER > $ADV
 
 cat $PAGEHEADER >> $INDEX
 cat $PAGEHEADER >> $ARCHIVE
+cat $PAGEHEADER >> $ADV
+
 echo ">> Generating PAGES"
 mkdir -p _output/page
 
@@ -33,8 +38,10 @@ done
 
 cat $PAGEFOOTER >> $INDEX
 cat $PAGEFOOTER >> $ARCHIVE
+cat $PAGEFOOTER >> $ADV
 
 count=1
+
 echo ">> Generating POSTS"
 for POST in $(ls -r _posts/); do 
     LINE=$(cat _posts/$POST | head -n 1)
@@ -56,7 +63,34 @@ for POST in $(ls -r _posts/); do
     cat _posts/$POST | perl $PREPROCESSOR >> $FILE
     cat $FOOTER >> $FILE
 done
-echo "<br><a href=\"archive\">Archive</a> " >> $INDEX
+
+count=1
+echo "<br />" >> $INDEX
+
+echo ">> Generating ADVISORIES"
+for A in $(ls -r _advisories/); do 
+    LINE=$(cat _advisories/$A | head -n 1)
+
+    mkdir -p _output/advisories/$A
+
+    LINK="<a href=\"/advisories/$A\">$A - $LINE</a><br />"
+    
+    if [ "$count" -le "$POSTCOUNT" ] 
+    then
+        echo $LINK >> $INDEX
+    fi
+    echo $LINK >> $ARCHIVE
+
+    count=$(($count+1))
+    FILE=_output/advisories/$A/index.html
+    cat $HEADER > $FILE
+    cat _advisories/$A | perl $PREPROCESSOR >> $FILE
+    cat $FOOTER >> $FILE
+done
+
+echo "<br><a href=\"archive\">posts</a> " >> $INDEX
+echo "| <a href=\"advisories\">advisories</a> " >> $INDEX
 
 cat $FOOTER >> $INDEX
 cat $FOOTER >> $ARCHIVE
+cat $FOOTER >> $ADV
